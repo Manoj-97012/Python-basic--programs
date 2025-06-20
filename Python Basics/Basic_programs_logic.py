191740f6cf696d1b32c2ec3e22001d44f838cc54
Manoj - 97012 <akulamanojkumar454@gmail.com>
2025-06-21 00:05:06 +0530

    https://github.com/users/Manoj-97012/projects/2

---
+++ Python Basics/Basic_programs_logic.py
@@ -0,0 +1,23 @@
+## Loop programs
+
+for i in range(1,100):
+    print(i)
+
+i = 100
+while i > 0:
+    print(i)
+
+## strings
+
+# palidrome
+s = s[::-1]
+
+
+
+## if else condition
+
+#even or odd
+num % 2 ==0
+
+#prime number or not 
+number > 1
